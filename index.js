c49b065b5f8e0d60baee4a26b542575eb4163de5
Your Name <you@example.com>
2024-03-06 22:03:01 -0500

    pintado de titulos en navegador

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-console.log("Hola mundo");
+
 const URL = "https://fakestoreapi.com/products";
+// let titulo = document.getElementById("titulo")
+// let precio = document.getElementById("precio")
+let productos = document.getElementById("productos")
+
+let tabla = "<div>";
 function getData(){
     fetch(URL)
     .then( response => response.json() )
-    .then( data => console.log("DATOS DE LA API: ", data) )
+    .then( data => {
+        console.log("Datos de la api: ", data)
+        for(let i = 0; i < data.length; i++ ){
+             //crear tag etiquetas html con js
+             let bloqueHtml = `<p>${data[i].title}</p>`;
+             console.log(bloqueHtml)
+             tabla += bloqueHtml;
+        }
+        tabla +="</div>";
+        productos.innerHTML = tabla;
+    })
+    
 }
 
 getData();
