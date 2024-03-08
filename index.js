eba4a322e4f71644b3c906950a42a532abd458bf
Your Name <you@example.com>
2024-03-07 22:01:27 -0500

    armador de card v1

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-
 const URL = "https://fakestoreapi.com/products";
-// let titulo = document.getElementById("titulo")
-// let precio = document.getElementById("precio")
 let productos = document.getElementById("productos")
-
-let tabla = "<div>";
+let tabla = "<div class='card'>";
 function getData(){
     fetch(URL)
     .then( response => response.json() )
     .then( data => {
         console.log("Datos de la api: ", data)
         for(let i = 0; i < data.length; i++ ){
-             //crear tag etiquetas html con js
-             let bloqueHtml = `<p>${data[i].title}</p>`;
-             console.log(bloqueHtml)
+             let bloqueHtml = 
+             `
+             <div class='card-item'>
+                <div class='cabecera'></div>
+                <img width="100px" src="${data[i].image}" />
+                <p>Titulo: ${data[i].title}</p>
+                <p>Precio: ${data[i].price}</p>
+                <p>Categ: ${data[i].category}</p>
+             </div>
+             `;
              tabla += bloqueHtml;
         }
         tabla +="</div>";
         productos.innerHTML = tabla;
     })
-    
 }
-
 getData();
